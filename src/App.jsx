02507dc084907c6ccc9b06f1ee3ee08ff2a75d75
Maciej Fec <maciejfec1996@gmail.com>
2023-04-01 18:10:02 +0100

    modify classe

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Quiz from './Quiz'
 
 function App() {
 
   return (
-    <div>
-      
+    <div className="flex-container w-100-mh-100vh centre">
+      <Quiz />
     </div>
   )
 }
