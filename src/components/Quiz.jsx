6bf65d8a5b1d0901058ce0f85bac04b73ba35d2c
Maciej Fec <maciejfec1996@gmail.com>
2023-04-15 17:38:52 +0100

    replace icon image with new quiz icon component. replace quiz header with new quiz header component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,17 +5,9 @@ import AnswersWire from './AnswersWire'
 import useTriviaApi from '../hooks/useTriviaApi'
 import useQuestionSelect from '../hooks/useQuestionSelect'
 import clickEvents from "../actions/clickEvents"
-import artIcon from "../assets/palette.png"
-import sportIcon from "../assets/bike.png"
-import generalKnowledgeIcon from "../assets/book.png"
-import scienceIcon from "../assets/chemistry.png"
-import foodIcon from "../assets/fast-food.png"
-import musicIcon from "../assets/guitar.png"
-import gepgraphyIcon from "../assets/map.png"
-import historyIcon from "../assets/parchment.png"
-import societyIcon from "../assets/sociology.png"
-import filmIcon from "../assets/watching.png"
 import Slider from './Slider'
+import CategoryIcon from './CategoryIcon'
+import QuizHeader from './QuizHeader'
 
 export const Context = createContext();
 
@@ -30,26 +22,13 @@ export default function Quiz() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(null)
   const [question, category, correctAnswer, allAnswers] = useQuestionSelect({questions, currentQuestionIndex})
   const allAnswerElRefs = useRef([])
-  const categoryIcons = {
-    "Arts & Literature": artIcon,
-    "Film & TV": filmIcon,
-    "Food & Drink": foodIcon,
-    "General Knowledge": generalKnowledgeIcon,
-    "Geography": gepgraphyIcon,
-    "History": historyIcon,
-    "Music": musicIcon,
-    "Science": scienceIcon,
-    "Society & Culture": societyIcon,
-    "Sport & Leisure": sportIcon
-  }
+  
   const selectedAnswer = useRef(null)
   const startGameBtnRef = useRef()
   
   if (!listOfLevels.includes(difficulty)) return (
     <div className="quiz-container flex-container">
-      <div className="quiz-header flex-container centre">
-        <h1 className="title no-text-select">Quiz App</h1>
-      </div>
+      <QuizHeader />
       <div className="quiz-body flex-container centre">
         <img src="brain.png" alt="brain icon with a question mark" className="category-icon" />
         <p className="question no-text-select">Select difficulty level</p>
@@ -66,9 +45,7 @@ export default function Quiz() {
 
   if(questions.length === 0) return (
     <div className="quiz-container">
-      <div className="quiz-header flex-container centre">
-        <h1 className="title no-text-select">Quiz App</h1>
-      </div>
+      <QuizHeader />
       <div className="quiz-body flex-container centre">
         <div className="loading"></div>
         <AnswersWire />
@@ -78,11 +55,9 @@ export default function Quiz() {
   
   return (
     <div className="quiz-container">
-      <div className="quiz-header flex-container centre">
-        <h1 className="title no-text-select">Quiz App</h1>
-      </div>
+      <QuizHeader />
       <div className="quiz-body flex-container centre">
-        <img src={categoryIcons[category]} alt="art colour palette" className="category-icon" />
+        <CategoryIcon category={category} />
         <p className="question no-text-select">{question}</p>
         <Context.Provider value={selectedAnswer}>
           <Answers answers={allAnswers} allAnswerElRefs={allAnswerElRefs} />
