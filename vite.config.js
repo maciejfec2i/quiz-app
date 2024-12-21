37826418ff5846a16fcf2dabc94b062c5caf8b7c
Maciej Fec <Maciej.Fec@2itesting.com>
2024-12-21 18:31:27 +0000

    fix: update spelling mistake from stricPort to strictPort

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +8,10 @@ export default defineConfig({
   preview: {
     port: 3000,
     strictPort: true,
-   },
+  },
   server: {
     port: 3000,
-    strickPort: true,
+    strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:3000"
-  }
-})
+  },
+});
