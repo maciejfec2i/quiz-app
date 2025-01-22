56f819b84ceac8a0bd2123f8b65b01109aa27c66
Maciej Fec <Maciej.Fec@2itesting.com>
2025-01-22 17:11:06 +0000

    feat: ET-IAC-7 initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "environment" {
+  description = "Environment type to create"
+  type        = string
+  default     = "staging"
+}
+
+locals {
+  ec2_instance_name = var.environment == "staging" ? "quiz-app-staging-instance-${random_integer.nine_digit.result}" : "quiz-app-prod-instance"
+}
+
+variable "ec2_instance_ami" {
+  description = "ID of the AWS EC2 instance AMI"
+  type        = string
+  default     = "ami-091f18e98bc129c4e"
+}
+
+variable "ec2_instance_type" {
+  description = "AWS EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
